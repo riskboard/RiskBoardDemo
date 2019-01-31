40bac40a950bb2caa1de7e7bf183c74752e765cd
Jonathan Pleban <opleban@gmail.com>
2019-01-31 16:57:34 -0500

    Updates

---
+++ precache-manifest.73403a7c47e98b6c478faba7d6fc755c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dd00d39dc8dc22c4a9cc",
+    "url": "/static/js/main.dd00d39d.chunk.js"
+  },
+  {
+    "revision": "5db2f81184a43e5ff238",
+    "url": "/static/js/1.5db2f811.chunk.js"
+  },
+  {
+    "revision": "dd00d39dc8dc22c4a9cc",
+    "url": "/static/css/main.9817d030.chunk.css"
+  },
+  {
+    "revision": "5db2f81184a43e5ff238",
+    "url": "/static/css/1.94c7f803.chunk.css"
+  },
+  {
+    "revision": "a61977336af4398e151007d04acef718",
+    "url": "/index.html"
+  }
+];
