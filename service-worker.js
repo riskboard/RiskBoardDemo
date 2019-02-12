94950b76ab2cbaed66392bc2f4838351bf2dba77
Jonathan Pleban <opleban@gmail.com>
2019-02-12 18:06:56 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/RiskBoardDemo/precache-manifest.8032b561227178f073e827db17d3652f.js"
+  "/RiskBoardDemo/precache-manifest.49b6047a9e54f728cdaf9137777f25b7.js"
 );
 
 workbox.clientsClaim();
