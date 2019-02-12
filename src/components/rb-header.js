03176954f9235c25546ace85b7be5e1d1809d043
Jonathan Pleban <opleban@gmail.com>
2019-02-12 12:03:45 -0500

    Updates favico

--- src/components/rb-header.js
+++ src/components/rb-header.js
@@ -6,7 +6,7 @@ export default () => {
 	return (
 		<header className="rb-header">
 			<div className="rb-header-logo">
-				<a href={`/${process.env.PUBLIC_URL}`}><img src={`${process.env.PUBLIC_URL}/riskLogo.jpg`} /></a>
+				<a href={`${process.env.PUBLIC_URL}`}><img src={`${process.env.PUBLIC_URL}/riskLogo.jpg`} /></a>
 			</div>
 		</header>
 	);
